1e88ef837facce2206d39b49d2110a7e0d21277a
Izzywizz <izzywizzz@gmail.com>
2016-10-18 09:15:37 +0100

    Initial Commit

---
+++ vouchking/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  vouchking
+//
+//  Created by Izzy on 18/10/2016.
+//  Copyright © 2016 Izzy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
