7d0ffa5964172b931e1845cac11986b28c1a7e4a
Izzywizz <izzywizzz@gmail.com>
2016-10-18 15:58:54 +0100

    Cleaned the code up in the testVC to use streamlined Data shared instance method

--- vouchking/TestViewController.m
+++ vouchking/TestViewController.m
@@ -19,9 +19,8 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    //testing whether the data shared instance method actually perisits data acrodd the applcation
-    Data *data = [Data sharedInstance];
-    NSLog(@"Shared Data test: %@", data.promotionsDict);
+    //Testing whether the data shared instance method actually perisits data across the applcation
+    NSLog(@"Shared Data test: %@",[Data sharedInstance].promotionsDict);
 }
 
 - (void)didReceiveMemoryWarning {
@@ -29,14 +28,5 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
